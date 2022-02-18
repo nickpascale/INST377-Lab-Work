410d13f768d6ceeaa24ba59a8c044be96bf256ba
nickpascale <98340796+nickpascale@users.noreply.github.com>
2022-02-18 00:15:40 -0500

    Commit 5

--- tetris/script.js
+++ tetris/script.js
@@ -66,7 +66,7 @@ function undraw() {
 }
 document.addEventListener('keyup', control)
 
-timerId = setInterval(moveDown, 1000);
+//timerId = setInterval(moveDown, 1000);
 
 function moveDown() {
   undraw();
@@ -131,7 +131,40 @@ function moveRight() {
     checkRotatedPosition()
     draw()
   }
-  
+
   const displaySquares = document.querySelectorAll('.mini-grid div')
   const displayWidth = 4
   const displayIndex = 0
+  let nextRandom = 0
+
+  const upNextTetrominoes = [
+    [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
+    [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
+    [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
+    [0, 1, displayWidth, displayWidth+1], //oTetromino
+    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
+  ]
+
+  function displayShape() {
+    //remove any trace of a tetromino form the entire grid
+    displaySquares.forEach(square => {
+      square.classList.remove('tetromino')
+      square.style.backgroundColor = ''
+    })
+    upNextTetrominoes[nextRandom].forEach( index => {
+      displaySquares[displayIndex + index].classList.add('tetromino')
+      displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom]
+    })
+  }
+
+  startBtn.addEventListener('click', () => {
+    if (timerId) {
+      clearInterval(timerId)
+      timerId = null
+    } else {
+      draw()
+      timerId = setInterval(moveDown, 1000)
+      nextRandom = Math.floor(Math.random()*theTetrominoes.length)
+      displayShape()
+    }
+  })
