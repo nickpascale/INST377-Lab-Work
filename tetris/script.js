a19010c590052e4ed1052186dbb014f7318a002f
nickpascale <98340796+nickpascale@users.noreply.github.com>
2022-02-17 23:58:07 -0500

    commit 3

--- tetris/script.js
+++ tetris/script.js
@@ -40,3 +40,83 @@ const iTetromino = [
   [1, width + 1, width * 2 + 1, width * 3 + 1],
   [width, width + 1, width + 2, width + 3]
 ];
+
+const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+let currentPosition = 4;
+const currentRotation = 0;
+
+let random = Math.floor(Math.random() * theTetrominoes.length);
+let current = theTetrominoes[random][currentRotation];
+
+function draw() {
+  current.forEach((index) => {
+    squares[currentPosition + index].classList.add('tetromino');
+    squares[currentPosition + index].style.backgroundColor = colors[random];
+  });
+}
+
+function undraw() {
+  current.forEach((index) => {
+    squares[currentPosition + index].classList.remove('tetromino');
+    squares[currentPosition + index].style.backgroundColor = '';
+  });
+}
+
+}
+document.addEventListener('keyup', control)
+
+timerId = setInterval(moveDown, 1000);
+
+function moveDown() {
+  undraw();
+  currentPosition += width;
+  draw();
+  freeze();
+}
+
+function freeze() {
+  if (current.some((index) => squares[currentPosition + index + width].classList.contains('taken'))) {
+    current.forEach((index) => squares[currentPosition + index].classList.add('taken'));
+    // start a new tetromino falling
+    random = nextRandom;
+    nextRandom = Math.floor(Math.random() * theTetrominoes.length);
+    current = theTetrominoes[random][currentRotation];
+    currentPosition = 4;
+    draw();
+    displayShape();
+    addScore();
+    gameOver();
+  }
+}
+function moveLeft() {
+  undraw();
+  const isAtLeftEdge = current.some((index) => (currentPosition + index) % width === 0);
+  if (!isAtLeftEdge) currentPosition -= 1;
+  if (current.some((index) => squares[currentPosition + index].classList.contains('taken'))) {
+    currentPosition += 1;
+  }
+  draw();
+}
+
+function control(e) {
+    if (e.keyCode === 37) {
+      moveLeft();
+    } else if (e.keyCode === 38) {
+      rotate();
+    } else if (e.keyCode === 39) {
+      moveRight();
+    } else if (e.keyCode === 40) {
+      moveDown();
+    }
+}
+
+function moveRight() {
+    undraw()
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+    if(!isAtRightEdge) currentPosition +=1
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition -=1
+    }
+    draw()
+  }
